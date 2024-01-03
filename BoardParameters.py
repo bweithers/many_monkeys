76727be125debb7f16c03e5b47ffb3ae8dd47ef2
BWeithers <36247821+bweithers@users.noreply.github.com>
2024-01-03 16:29:33 -0600

    Added parameters file to maintain in one place

---
+++ BoardParameters.py
@@ -0,0 +1,19 @@
+prop_set_sizes = {
+    'brown': 2,
+    'light blue': 3, 
+    'pink': 3,
+    'orange': 3,
+    'red': 3,
+    'yellow': 3,
+    'green': 3,
+    'blue': 2
+}
+house_prices = {'brown': 50,
+                'light blue': 50,
+                'pink': 100,
+                'orange': 100,
+                'red': 150,
+                'yellow': 150,
+                'green': 200,
+                'blue': 200
+            }
